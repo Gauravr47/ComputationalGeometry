c1180bc941ae4dd5609715e84973e0aabbfeca20
gauravr47 <gauravrathod47@gmail.com>
2024-06-20 23:50:02 -0400

    include and separate classes

--- IntersectionsEigen/src/main.cpp
+++ IntersectionsEigen/src/main.cpp
@@ -1,71 +1,51 @@
 #include "primitives.h"
+#include "solid.h"
+#include "contour.h"
 //#include "OpenGLFunctions.h"
 using namespace std;
 
 int main() {
 	try {
-		Solid s("testFiles/cube_hole.stl");
-		//std::ofstream ofs("test.log");
-		//std::cout << "\n Archiving";
-		//// save data to archive
-		//{
-		//	boost::archive::text_oarchive oa(ofs);
-		//	// write class instance to archive
-		//	oa << s;
-		//	// archive and stream closed when destructors are called
-		//}
+		Solid s("testFiles/cube_edge_hole.stl");
 
+		box bounds = s.getBoundingBox();
 		vector<point3d> intersections;
 		point3d planeNormal;
-		planeNormal = getPlaneNormal(0, 0, 45);
-		point3d planePoint(50, 0.0, -50.0);
+		planeNormal = getPlaneNormal(90, 45);
+		point3d planePoint(planeNormal* -100);
 		point3d lightUnitVec;
-		lightUnitVec = getPlaneNormal(0.0, 0.0, 45);
+		lightUnitVec = -1 * planeNormal;
 		point3d zPlane;
-		zPlane = getPlaneNormal(0, 0, 90);
-		vector<edge> edges = std::move(s.getEdges());
-		slicingPlaneIntersection(planeNormal, planePoint, edges, intersections);
-		std::cout << "\n Done intersecting";
+		zPlane = getPlaneNormal(90, 90);
+
+
 		Contour C(segSolidIntersection(lightUnitVec, s), s);
-		auto projRings = project(planeNormal, planePoint, lightUnitVec, C);
+		vector<ring> rings = C.getRings();
+		auto projRings = project(planeNormal, planePoint, lightUnitVec, rings , s);
 
-		vector<point3d> hull;
-		boost::geometry::convex_hull(intersections, hull);
-
+		//Save to svg
 		bg::model::polygon<point2d> hull2d;
 		auto tranAngle = planeNormal.dot(zPlane);
 		auto tranAxis = planeNormal.cross(zPlane);
+
 		Eigen::Transform<float,3, Eigen::Affine> trans(Eigen::AngleAxis(acos(tranAngle), tranAxis));
+		int i = 0;
 		for (auto& loop : projRings) {
 			for (auto const& p : loop) {
-				point3d temp = trans * p;
-				bg::append(hull2d.outer(), point2d{temp.x(), temp.y()});
+				point3d temp = trans* p;
+				if(i == 0)
+					bg::append(hull2d.outer(), point2d{temp.x(), temp.y()});
+				else {
+					bg::append(hull2d.inners().back(), point2d{ temp.x(), temp.y() });
+				}
 			}
+			i++;
+			hull2d.inners().resize(1);
 		}
-
+		
 		string style = "fill-opacity:0.5;fill:rgb(153,204,0);stroke:rgb(153,204,0);stroke-width:2";
 		string svg_file = "hull.svg";
 		create_svg(svg_file, hull2d, style);
-
-		std::cout << "\n Hull";
-		for (auto const intS : hull) {
-			std::cout << "\n\n" << intS;
-		}
-
-
-
-		//// ... some time later restore the class instance to its orginal state
-		//std::cout << "\n Retreiving archive";
-		//Solid newg;
-		//{
-		//	// create and open an archive for input
-		//	std::ifstream ifs("test.log");
-		//	boost::archive::text_iarchive ia(ifs);
-		//	// read class state from archive
-		//	ia >> newg;
-		//	// archive and stream closed when destructors are called
-		//}
-
 
 	}
 	catch (exception& e) {
--- IntersectionsEigen/src/primitives.cpp
+++ IntersectionsEigen/src/primitives.cpp
@@ -1,32 +1,8 @@
 #include "primitives.h"
+#include "solid.h"
+#include "contour.h"
 
-string to_string(const point3d& p) {
-    string s = to_string(p[0]) + "," + to_string(p[1]) + "," + to_string(p[2]);
-    return s;
-}
-
-void fromEdgeStringToIndixes(string s, vector<int>& indices) {
-    std::string delimiter = ",";
-    size_t pos = 0;
-    std::string token;
-    while ((pos = s.find(delimiter)) != std::string::npos) {
-        token = s.substr(0, pos);
-        indices.push_back(stoi(token));
-        s.erase(0, pos + delimiter.length());
-    }
-    indices.push_back(stoi(s));
-    return;
-}
-
-string fromIndicesToEdgeString(int one, int two)
-{
-    if (one < two) {
-        return to_string(one) + "," + to_string(two);
-    }
-    else {
-        return to_string(two) + "," + to_string(one);
-    }
-};
+//helpers
 
 edgeIndex createEdge(int one, int two) {
     if (one > two) {
@@ -37,167 +13,15 @@ edgeIndex createEdge(int one, int two) {
     return edgeIndex{one, two};
 };
 
-vector3d getPlaneNormal(float alpha, float beta, float theta) {
-    vector3d result;
-    result[0] = cos(alpha * M_PI /180);
-    result[1] = cos(beta * M_PI / 180);
-    result[2] = cos(theta * M_PI / 180);
-    return result;
-}
-
-// Solid class methods
-void Solid::readBinarySTL(const char* filename)
-{
-
-    STL_READER_COND_THROW(StlFileHasASCIIFormat(filename), "Only binary files are supported " << filename);
-
-    ifstream in(filename, ios::binary);
-    STL_READER_COND_THROW(!in, "Couldnt open file " << filename);
-
-    std::unordered_map<point3d, int, matrix_hash<point3d>> mp;
-    char stl_header[80];
-    in.read(stl_header, 80);
-    STL_READER_COND_THROW(!in, "Error while parsing binary stl header in file " << filename);
-
-    unsigned int numTris = 0;
-    in.read((char*)&numTris, 4);
-    STL_READER_COND_THROW(!in, "Couldnt determine number of triangles in binary stl file " << filename);
-
-    unsigned int index = 0;
-
-    for (unsigned int tri = 0; tri < numTris; ++tri) {
-        float d[12];
-        in.read((char*)d, 12 * 4);
-        STL_READER_COND_THROW(!in, "Error while parsing trianlge in binary stl file " << filename);
-
-        normals.push_back(point3d(d[0], d[1], d[2]));
-        faceIndex fIndex;
-        for (size_t ivrt = 1; ivrt < 4; ++ivrt) {
-
-            point3d vert(d[ivrt * 3 + 0], d[ivrt * 3 + 1], d[ivrt * 3 + 2]);
-            //string vertS = to_string(vert);
-            if (mp.find(vert) != mp.end()) {
-                fIndex[ivrt - 1] = mp[vert];
-            }
-            else {
-                vertices.push_back(vert);
-                mp[vert] = index;
-                fIndex[ivrt - 1] = index;
-                index++;
-            }
-        }
-
-        for (std::vector<int>::size_type i = 0; i < fIndex.size(); i++)
-        {
-            edgeIndex edge = createEdge(fIndex[i % fIndex.size()], fIndex[(i + 1) % fIndex.size()]);
-            edgeFaceAdj[edge].push_back(tri);
-        }
-
-        faceIndices.push_back(fIndex);
-        char addData[2];
-        in.read(addData, 2);
-        STL_READER_COND_THROW(!in, "Error while parsing additional triangle data in binary stl file " << filename);
+bool isCollinear(const point3d& p, const point3d& q, const point3d& r) {
+    vector3d pq = q - p;
+    vector3d pr = r - p;
+    vector3d zero = vector3d::Zero();
+    if (isApproxEqual(pq.cross(pr), zero)) {
+        return true;
     }
-
-    findShells();
-    findInternalGeometry();
-    return;
-
+    return false;
 }
-
-void Solid::findShells() {
-    set<int> seen;
-    int numSolids = 0;
-    for (auto& edgefacesPair : edgeFaceAdj) {
-        edgeIndex edge = edgefacesPair.first;
-        vector<int> faces = edgefacesPair.second;
-        if (seen.find(faces[0]) == seen.end() || seen.find(faces[1]) == seen.end()) {
-            shells.push_back(set<int>{});
-            box aabb = findShellHelper(seen, faces);
-            aabbTree.insert(make_pair(aabb, numSolids++));
-            internalGeometry.push_back(false);
-        }
-    }
-    return;
-}
-
-box Solid::findShellHelper(set<int>& seen, vector<int>& currfaces) {
-    queue<int> queue;
-    point3d minC{ (float)INT_MAX ,(float)INT_MAX ,(float)INT_MAX }, maxC{ (float)INT_MIN ,(float)INT_MIN ,(float)INT_MIN };
-    for (auto FI : currfaces) {
-        queue.push(FI);
-    }
-    while (!queue.empty()) {
-        int currentLength = queue.size();
-        set<int> toAdd;
-        for (int i = 0; i < currentLength; i++) {
-            int FI = queue.front();
-            queue.pop();
-            if (seen.find(FI) == seen.end()) {
-                seen.insert(FI);
-                shells.back().insert(FI);
-                faceIndex fIndex = faceIndices[FI];
-                for (int i = 0; i < (int)fIndex.size(); i++)
-                {
-                    edgeIndex edge = createEdge(fIndex[i % fIndex.size()], fIndex[(i + 1) % fIndex.size()]);
-                    point3d vert = vertices[fIndex[i]];
-                    //find mix max to create a box
-                    minC[0] = min(minC[0], vert[0]);
-                    maxC[0] = max(maxC[0], vert[0]);
-                    minC[1] = min(minC[1], vert[1]);
-                    maxC[1] = max(maxC[1], vert[1]);
-                    minC[2] = min(minC[2], vert[2]);
-                    maxC[2] = max(maxC[2], vert[2]);
-                    if (edgeFaceAdj.find(edge) != edgeFaceAdj.end()) {
-                        vector<int> newFace = edgeFaceAdj[edge];
-                        for (auto newFI : newFace) {
-                            if (seen.find(newFI) == seen.end()) {
-                                toAdd.insert(newFI);
-                            }
-                        }
-                    }
-
-                }
-            }
-
-        }
-        for (auto t : toAdd) {
-            queue.push(t);
-        }
-    }
-    box aabb(minC, maxC);
-    return aabb;
-}
-
-bool Solid::StlFileHasASCIIFormat(const char* filename)
-{
-    using namespace std;
-    ifstream in(filename);
-    STL_READER_COND_THROW(!in, "Couldnt open file " << filename);
-
-    char chars[256];
-    in.read(chars, 256);
-    string buffer(chars, in.gcount());
-    transform(buffer.begin(), buffer.end(), buffer.begin(), ::tolower);
-    return buffer.find("solid") != string::npos &&
-        buffer.find("\n") != string::npos &&
-        buffer.find("facet") != string::npos &&
-        buffer.find("normal") != string::npos;
-};
-
-void Solid::findInternalGeometry() {
-    for (auto const& v : aabbTree) {
-
-        std::vector<std::pair<box, unsigned>> returned_values;
-        std::copy(aabbTree.qbegin(bgi::contains(v.first)), aabbTree.qend(), std::back_inserter(returned_values));
-        if (returned_values.size() % 2 == 0) {
-            internalGeometry[v.second] = true;
-        }
-    }
-}
-
-
-//eigen primitives intersections
 
 vector3d unitVec(const point3d& p, const point3d& q)
 {
@@ -209,6 +33,31 @@ vector3d unitVec(const vector3d& pq)
     return pq / pq.norm();
 }
 
+vector3d getPlaneNormal(float alpha, float beta) {
+    vector3d result;
+    result[0] = cos(alpha * M_PI /180)* cos(beta * M_PI / 180);
+    result[1] = sin(alpha * M_PI / 180)*cos(beta * M_PI / 180);
+    result[2] = sin(beta * M_PI / 180);
+    return result;
+}
+
+bool isApproxEqual(const vector3d& u, const vector3d& v) {
+    return (fabs(u[0] - v[0]) < EPSILON) && (fabs(u[1] - v[1]) < EPSILON) && (fabs(u[2] - v[2]) < EPSILON);
+}
+
+void updateMinMax(const point3d& vert, point3d& minC, point3d& maxC) {
+
+    minC[0] = min(minC[0], vert[0]);
+    maxC[0] = max(maxC[0], vert[0]);
+    minC[1] = min(minC[1], vert[1]);
+    maxC[1] = max(maxC[1], vert[1]);
+    minC[2] = min(minC[2], vert[2]);
+    maxC[2] = max(maxC[2], vert[2]);
+    return;
+}
+
+
+//Intersections
 point3d segSegIntersectEigen(const point3d& p, const point3d& q, const point3d& r, const point3d& s) {
     using Line2 = Eigen::Hyperplane<float, 2>;
     Line2 pq = Line2::Through(p.head<2>(), q.head<2>());
@@ -220,13 +69,13 @@ point3d segSegIntersectEigen(const point3d& p, const point3d& q, const point3d& 
 
 char segPlaneIntersectEigen(const vector3d& planeNormal, const point3d& planePoint, const point3d& p, const point3d& q, point3d& found) {
 
-    float denom = planeNormal.transpose() * (p - q);
+    float denom = planeNormal.dot(p - q);
     vector3d p0l0 = planePoint - p;
     float num = p0l0.dot(planeNormal);
     if (denom > 1e-6 || denom < -1e-6)       
     {
         float t = num / denom;
-        found = (t * (p - q)) + p;
+        found = (t * unitVec(p - q)) + p;
         return 'i';
     }
     else {
@@ -254,10 +103,6 @@ void slicingPlaneIntersection(const vector3d& planeNormal, const point3d& planeP
     }
 }
 
-void segSolidIntersection(const point3d& p, const point3d& q, Solid& solid) {
-    return;
-}
-
 vector<unordered_set<edgeIndex, matrix_hash<edgeIndex>>> segSolidIntersection(const vector3d& ray, Solid& solid) {
     //for shells != internal
     //dot product of face normal and planeNormal
@@ -265,8 +110,8 @@ vector<unordered_set<edgeIndex, matrix_hash<edgeIndex>>> segSolidIntersection(co
     //get the edges, check for triangles next 
     //if in seen, remove common edge from edges output
     //
-    vector3d backRay = -1 * ray;
     vector<unordered_set<edgeIndex,matrix_hash<edgeIndex>>> edgesToProj;
+    vector3d backRay = -1*ray;
     for(int i = 0; i < solid.shells.size(); i++) {
         if (!solid.internalGeometry[i]) {
             set<int> seen;
@@ -278,118 +123,6 @@ vector<unordered_set<edgeIndex, matrix_hash<edgeIndex>>> segSolidIntersection(co
         }
     }
     return edgesToProj;
-}
-
-// read file
-void findSolids(std::unordered_map<edgeIndex, std::vector<int>, matrix_hash<edgeIndex>> & edgeFaceAdj, vector<faceIndex>& faceIndices, vector<set<int>>& solids) {
-    set<int> seen;
-    int numSolids = 0;
-    for (auto& edgefacesPair : edgeFaceAdj) {
-        edgeIndex edge = edgefacesPair.first;
-        vector<int> faces = edgefacesPair.second;
-        if (seen.find(faces[0]) == seen.end() || seen.find(faces[1]) == seen.end()) {
-            solids.push_back(set<int>{});
-            numSolids++;
-            bfs(edgeFaceAdj, faceIndices, seen, solids, faces);
-        }
-    }
-    return;
-}
-
-void readBinarySTL(const char* filename, vector<point3d>& vertices, vector<point3d>& normals, vector<faceIndex>& faceIndices, unordered_map<edgeIndex, vector<int>, matrix_hash<edgeIndex>>& edgeFaceAdj, std::vector<set<int>>& solids)
-{
-
-    STL_READER_COND_THROW(StlFileHasASCIIFormat(filename), "Only binary files are supported " << filename);
-
-    ifstream in(filename, ios::binary);
-    STL_READER_COND_THROW(!in, "Couldnt open file " << filename);
-
-    std::unordered_map<point3d, int, matrix_hash<point3d>> mp;
-    char stl_header[80];
-    in.read(stl_header, 80);
-    STL_READER_COND_THROW(!in, "Error while parsing binary stl header in file " << filename);
-
-    unsigned int numTris = 0;
-    in.read((char*)&numTris, 4);
-    STL_READER_COND_THROW(!in, "Couldnt determine number of triangles in binary stl file " << filename);
-
-    unsigned int index = 0;
-
-    for (unsigned int tri = 0; tri < numTris; ++tri) {
-        float d[12];
-        in.read((char*)d, 12 * 4);
-        STL_READER_COND_THROW(!in, "Error while parsing trianlge in binary stl file " << filename);
-
-        normals.push_back(point3d(d[0], d[1], d[2]));
-        faceIndex fIndex;
-        for (size_t ivrt = 1; ivrt < 4; ++ivrt) {
-
-            point3d vert(d[ivrt * 3 + 0], d[ivrt * 3 + 1], d[ivrt * 3 + 2]);
-            //string vertS = to_string(vert);
-            if (mp.find(vert) != mp.end()) {
-                fIndex[ivrt - 1] = mp[vert];
-            }
-            else {
-                vertices.push_back(vert);
-                mp[vert] = index;
-                fIndex[ivrt - 1] = index;
-                index++;
-            }
-        }
-
-        for (std::vector<int>::size_type i = 0; i < fIndex.size(); i++)
-        {
-            edgeIndex edge = createEdge(fIndex[i % fIndex.size()], fIndex[(i + 1) % fIndex.size()]);
-            edgeFaceAdj[edge].push_back(tri);
-        }
-
-        faceIndices.push_back(fIndex);
-        char addData[2];
-        in.read(addData, 2);
-        STL_READER_COND_THROW(!in, "Error while parsing additional triangle data in binary stl file " << filename);
-    }
-
-    findSolids(edgeFaceAdj, faceIndices, solids);
-    return;
-};
-
-void bfs(std::unordered_map<edgeIndex, std::vector<int>, matrix_hash<edgeIndex>> & edgeFaceAdj, vector<faceIndex>& faceIndices, set<int>& seen, vector<set<int>>& solids, vector<int>& currfaces) {
-    queue<int> queue;
-    for (auto FI : currfaces) {
-        queue.push(FI);
-    }
-    while (!queue.empty()) {
-        int currentLength = queue.size();
-        set<int> toAdd;
-        for (int i = 0; i < currentLength; i++) {
-            int FI = queue.front();
-            queue.pop();
-            if (seen.find(FI) == seen.end()) {
-                seen.insert(FI);
-                solids.back().insert(FI);
-                faceIndex fIndex = faceIndices[FI];
-                for (int i = 0; i <(int) fIndex.size(); i++)
-                {
-                    edgeIndex edge = createEdge(fIndex[i % fIndex.size()], fIndex[(i+1) % fIndex.size()]);
-                    //point3d vert = vertices(fIndex[i]);
-                    if (edgeFaceAdj.find(edge) != edgeFaceAdj.end()) {
-                        vector<int> newFace = edgeFaceAdj[edge];
-                        for (auto newFI : newFace) {
-                            if (seen.find(newFI) == seen.end()) {
-                                toAdd.insert(newFI);
-                            }
-                        }
-                    } 
-
-                }
-            }
-
-        }
-        for (auto t : toAdd) {
-            queue.push(t);
-        }
-    }
-    return;
 }
 
 void bfs_intersection(const vector3d& ray, std::unordered_map<edgeIndex, std::vector<int>, matrix_hash<edgeIndex>>& edgeFaceAdj, std::vector<faceIndex>& faceIndices, std::vector<point3d>& normals, std::set<int>& seen, std::vector<std::unordered_set<edgeIndex, matrix_hash<edgeIndex>>>& edgesToProj, int startFace) {
@@ -406,7 +139,7 @@ void bfs_intersection(const vector3d& ray, std::unordered_map<edgeIndex, std::ve
             if (seen.find(FI) == seen.end()) {
                 seen.insert(FI);
                 double t = ray.dot(normals[FI]);
-                if (t <=PIbyTwo) {
+                if ((std::truncf(t* 1e10)/ 1e-10) <=PIbyTwo) {
                     faceIndex fIndex = faceIndices[FI];
                     for (int i = 0; i < (int)fIndex.size(); i++)
                     {
@@ -417,9 +150,11 @@ void bfs_intersection(const vector3d& ray, std::unordered_map<edgeIndex, std::ve
                             bool fullLit = true;
                             for (auto& newFace : newFaces) {
                                 if (newFace != FI) {
-                                    bool halfLit = (ray.dot(normals[newFace]) <= PIbyTwo);
+                                    double t_ = ray.dot(normals[newFace]);
+                                    bool halfLit = ((std::truncf(t_ * 1e10) / 1e-10) <= PIbyTwo);
                                     fullLit = fullLit && halfLit;
                                     if (!fullLit) {
+
                                         edgesToProjSet.insert(edgeI);
                                     }
                                     else {
@@ -444,7 +179,6 @@ void bfs_intersection(const vector3d& ray, std::unordered_map<edgeIndex, std::ve
     return;
 }
 
-
 vector<ring> project(const vector3d& planeNormal, const point3d& planePoint, const vector3d& lightUnitVec, Contour& contour) {
     vector<ring> projRings;
     auto rings = contour.getRings();
@@ -454,10 +188,40 @@ vector<ring> project(const vector3d& planeNormal, const point3d& planePoint, con
             point3d found;
             char f = segPlaneIntersectEigen(planeNormal, planePoint, p, p + 1 * lightUnitVec, found);
             if (f != '0') {
+                //ray trace from found to p with distance d, 
+                //intersect with shells external, if distance is  <d, then collision, do not add
                 temp.push_back(found);
             }
         }
-        projRings.push_back(temp);
+        if (!temp.empty()) {
+            projRings.push_back(temp);
+        }
+    }
+    return projRings;
+}
+
+vector<ring> project(const vector3d& planeNormal, const point3d& planePoint, const vector3d& lightUnitVec, vector<ring>& rings, Solid &solid) {
+    vector<ring> projRings;
+    vector<vector<double>> distances;
+    for (int i = 0; i < rings.size(); i++) {
+        ring temp;
+        vector<double> dist_temp;
+        for (auto p : rings[i]) {
+            point3d found;
+            char f = segPlaneIntersectEigen(planeNormal, planePoint, p, p + 1 * lightUnitVec, found);
+            if (f != '0') {
+                //ray trace from found to p with distance d, 
+                //intersect with shells external, if distance is  <d, then collision, do not add
+                float distance = (found - p).norm();
+                cout << "\n" << found << " distance" << to_string(distance);
+                temp.push_back(found);
+                dist_temp.push_back(distance);
+            }
+        }
+        if (!temp.empty()) {
+            projRings.push_back(temp);
+            distances.push_back(dist_temp);
+        }
     }
     return projRings;
 }
